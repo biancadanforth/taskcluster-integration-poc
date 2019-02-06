bae9e32129f5111d0b09694b43cf8ae6c36ece22
Bianca Danforth <biancadanforth@gmail.com>
2019-02-06 13:35:00 -0800

    Fix #8: Run custom mochitest locally and on Try server
    Runs the custom mochitest, `browser_experiment_installed_check.js`, which simply checks if the add-on is installed in Firefox.
    
    It is possible to do this thanks to Bug 1517083, which just landed, which has the build system look in a new ./testing/extensions/ directory for tests to run.

--- bin/commit_xpi.sh
+++ bin/commit_xpi.sh
@@ -4,27 +4,40 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# Clones Firefox, copies the XPI to a test directory and commits the change to Mercurial.
+# Clones Firefox, copies the XPI and any custom tests to testing directories
+# and commits the change to Mercurial.
 
 set -e
 
-# Note: If clone fails with error: `abort: unexpected response from remote server: empty string.`,
-# that clonebundle may be corrupted. Try another URL to confirm and contact #vcs in IRC.
+# Note: If clone fails with error: `abort: unexpected response from remote
+# server: empty string.`, that clonebundle may be corrupted. Try another URL
+# to confirm and contact #vcs in IRC.
 echo ">>> Clone Firefox"
 hg clone https://hg.mozilla.org/mozilla-central
 
-# Per bugs 1451159/1458571, when copied here, the extension will be installed with the testing profile.
+# Per bugs 1451159/1458571, when copied here, the extension will be installed
+# with the testing profile.
 echo ">>> Copy XPI (or ZIP) to test dir"
 cp /repo/web-ext-artifacts/* /repo/mozilla-central/testing/profiles/common/extensions/
 
-# TODO - depends on Bug 1517083, else must edit an existing moz.build file programmatically ~
-# echo ">>> Copy over .mozconfig to enable artifact builds locally"
-# echo ">>> Copy test files over to Firefox"
-# echo ">>> Build Firefox"
-# echo ">>> Verify with custom local mochitest that extension is installed"
+# Run custom mochitest on Docker image before pushing to Try (See bug 1517083)
+echo ">>> Create .mozconfig to enable artifact builds locally"
+if [ ! -f /repo/mozilla-central/.mozconfig ]
+then
+  cat <<EOF > /repo/mozilla-central/.mozconfig
+# Automatically download and use compiled C++ components:
+ac_add_options --enable-artifact-builds
+EOF
+fi
+echo ">>> Copy test files over to Firefox"
+cp -r /repo/test/* /repo/mozilla-central/testing/extensions/
+echo ">>> Build Firefox"
+cd mozilla-central
+./mach build
+echo ">>> Verify with custom local mochitest that extension is installed"
+./mach test testing/extensions
 
 # The diff needs to be checked in to run on the Try server
 echo ">>> Commit diff to hg"
-cd mozilla-central
 hg add .
 hg commit -m "Temporary commit to run extension on the Try server"
